22d8dd7e7c6dc7071775a333b91aae4576a16870
vpolaris <szfd9g@live.fr>
2021-03-21 11:16:04 +0100

    Signed-off-by: vpolaris <szfd9g@live.fr>
    Bugfix
    Enhancement
      + Create bitwarden user on host with container manangment rights only
      + Build the image with the bitwarden user

--- setup.sh
+++ setup.sh
@@ -3,14 +3,29 @@ ADMTKN="$(tr -cd [:alnum:] < /dev/urandom | fold -w 48 | head -n 1)"
 ADMINPASS="$(tr -cd [:alnum:] < /dev/urandom | fold -w 16 | head -n 1)"
 DOMAIN="vault.bitwarden.lan"
 HTTPS="443"
-DATADIR="${HOME}/.persitent_storage/"
+DATADIR="/home/bitwarden/.persitent_storage/"
 VERSION="1.00"
+
+if [ "${EUID}" -ne 0 ]; then 
+	printf "your are not root user. To proceed run\nsudo ./setup.sh\n"
+	exit 0
+fi
 
 read -e -p "Enter ADMIN TOKEN:" -i "${ADMTKN}" ADMTKN
 read -e -p "Enter ADMIN Password:" -i "${ADMINPASS}" ADMINPASS
 read -e -p "Enter Domain name for Vault Website:" -i "${DOMAIN}" DOMAIN
 read -e -p "Enter https port number:" -i "${HTTPS}" HTTPS
 read -e -p "Enter tag version:" -i "${VERSION}" VERSION
+
+
+if [[ $(id -u bitwarden) -eq 10500 ]]; then 
+	printf "User bitwarden already exists\n"; 
+else
+	printf "Creation of bitwarden user\n";
+	adduser -u 10500 -G docker --shell /bin/bash --comment "Bitwarden RS User Container" --user-group bitwarden
+	loginctl enable-linger 10500
+	systemctl start user@10500.service
+fi
 
 if ! [ -f  "./layer.tar" ]; then
   proc="$(uname -m)"
@@ -41,43 +56,60 @@ if ! [ -f  "./layer.tar" ]; then
   
 fi
 
-if ! [ -d  "${DATADIR}" ]; then
-	printf  "${DATADIR} does not exist, please provide a location for vault store\n "
-	read -e -p "Enter Base DATA directory:" -i "${DATADIR}" DATADIR
+if ! [ -d  "${DATADIR}" ]; then 
+	printf  "${DATADIR} does not exist, please provide a location for vault store\n "; 
+	read -e -p "Enter Base DATA directory:" -i "${DATADIR}" DATADIR; 
 fi
 
 if ! [ -d  "${DATADIR}/bitwarden" ]; then
-	mkdir "${DATADIR}/bitwarden"
+	mkdir -p "${DATADIR}/bitwarden/project"
 fi
 
 if ! [ -d  "${DATADIR}/bitwarden/certs" ]; then
-	mkdir "${DATADIR}/bitwarden/certs"
-else
-	if ! [ -f  "${DATADIR}/bitwarden/certs/CA-Bitwarden.pem" ]; then
-		openssl req -new -x509 -nodes -days 7300 -outform PEM -newkey rsa:4096 -sha256 \
-		-keyout ${DATADIR}/bitwarden/certs/CA-Bitwarden.key \
-		-out ${DATADIR}/bitwarden/certs/CA-Bitwarden.pem \
-		-subj "/CN=CA Bitwarden/emailAddress=admin@${DOMAIN}/C=FR/ST=IDF/L=Paris/O=Podman Inc/OU=Podman builder"; 
-		openssl req -nodes -newkey rsa:2048 -sha256 \
-		-keyout ${DATADIR}/bitwarden/certs/bitwarden.key \
-		-out ${DATADIR}/bitwarden/certs/bitwarden.csr \
-		-subj "/CN=vault.bitwarden.lan/emailAddress=admin@${DOMAIN}/C=FR/ST=IDF/L=Paris/O=Podman Inc/OU=Podman builder";
-	fi
-	
-	if ! [ -f  "${DATADIR}/bitwarden/certs/bitwarden.pem" ]; then
-		openssl x509 -req -outform PEM -CAcreateserial \
-		-in ${DATADIR}/bitwarden/certs/bitwarden.csr \
-		-CA ${DATADIR}/bitwarden/certs/CA-Bitwarden.pem \
-		-CAkey ${DATADIR}/bitwarden/certs/CA-Bitwarden.key \
-		-out ${DATADIR}/bitwarden/certs/bitwarden.pem
-	fi
+	mkdir -p "${DATADIR}/bitwarden/certs"
 fi	
 
 if ! [ -d  "${DATADIR}/bitwarden/logs" ]; then
 	mkdir -p "${DATADIR}/bitwarden/logs/{bitwarden,httpd}}"
 fi
 
+if ! [ -f  "${DATADIR}/bitwarden/certs/CA-Bitwarden.pem" ]; then
+	openssl req -new -x509 -nodes -days 7300 -outform PEM -newkey rsa:4096 -sha256 \
+	-keyout ${DATADIR}/bitwarden/certs/CA-Bitwarden.key \
+	-out ${DATADIR}/bitwarden/certs/CA-Bitwarden.pem \
+	-subj "/CN=CA Bitwarden/emailAddress=admin@${DOMAIN}/C=FR/ST=IDF/L=Paris/O=Podman Inc/OU=Podman builder"; 
+	openssl req -nodes -newkey rsa:2048 -sha256 \
+	-keyout ${DATADIR}/bitwarden/certs/bitwarden.key \
+	-out ${DATADIR}/bitwarden/certs/bitwarden.csr \
+	-subj "/CN=vault.bitwarden.lan/emailAddress=admin@${DOMAIN}/C=FR/ST=IDF/L=Paris/O=Podman Inc/OU=Podman builder";
+fi
 
+if ! [ -f  "${DATADIR}/bitwarden/certs/bitwarden.pem" ]; then
+	openssl x509 -req -outform PEM -CAcreateserial \
+	-in ${DATADIR}/bitwarden/certs/bitwarden.csr \
+	-CA ${DATADIR}/bitwarden/certs/CA-Bitwarden.pem \
+	-CAkey ${DATADIR}/bitwarden/certs/CA-Bitwarden.key \
+	-out ${DATADIR}/bitwarden/certs/bitwarden.pem
+fi
+
+if ! [ -d  "/home/bitwarden/.config" ]; then
+	mkdir -p "/home/bitwarden/.config/containers"
+fi
+
+if ! [ -d  "/home/bitwarden/.local/share/containers" ]; then
+	mkdir -p "/home/bitwarden/.local/share/containers"
+fi
+
+cp -r . "${DATADIR}/bitwarden/project"
+chown -R bitwarden: "${DATADIR}" "/home/bitwarden/.config" "/home/bitwarden/.local"; chmod -R 770 "${DATADIR}"
+
+semanage fcontext -a -e "/var/lib/containers" "/home/bitwarden/.local/share/containers"
+semanage fcontext -a -e "/etc/containers" "/home/bitwarden/.config/containers"
+restorecon -R /home/bitwarden/.local/share/containers "/home/bitwarden/.config/containers"
+semanage fcontext -a -f a -t container_file_t "${DATADIR}/bitwarden(/.*)?"
+restorecon -R "{DATADIR}/bitwarden"
+sudo su - bitwarden -c 'podman info'
+pause
 
 export ADMTKN="${ADMTKN}" DOMAIN="${DOMAIN}" HTTPS="${HTTPS}"
 envsubst '${ADMTKN} ${DOMAIN}'< .env.txt > .env
@@ -94,6 +126,28 @@ if [[ ${OS}=="" ]]; then
     printf "${OS}" > os.version
   fi
 fi
-printf "Executed Command :\n"
-printf "podman build --squash-all -t "$(tr '[:upper:]' '[:lower:]' <<< ${OS})"-bitwarden:${VERSION} --build-arg admpass=${ADMINPASS} --build-arg OS=${OS} -v ${DATADIR}/bitwarden:/var/lib/bitwarden:Z -f Dockerfile"
-sudo podman build --squash-all -t "$(tr '[:upper:]' '[:lower:]' <<< ${OS})"-bitwarden:${VERSION} --build-arg admpass=${ADMINPASS} --build-arg OS=${OS} -v ${DATADIR}/bitwarden:/var/lib/bitwarden:Z -f Dockerfile
+TAGNAME="$(tr '[:upper:]' '[:lower:]' <<< ${OS})"-bitwarden:"${VERSION}"
+printf "Do you want to start build process (y|n)\n"
+read -e -p "Enter your answer " -i "y" VALIDATE
+case "${VALIDATE}" in
+	"y")
+	printf "Executed Command :\n"
+	printf "sudo su - bitwarden -c podman build --squash-all -t ${TAGNAME} --build-arg admpass=${ADMINPASS} --build-arg OS=${OS} --build-arg HTTPS=${HTTPS} -v ${DATADIR}/bitwarden:/var/lib/bitwarden:Z -f ${DATADIR}bitwarden/project\n"
+	sudo su - bitwarden -c "podman build --squash-all -t ${TAGNAME} --build-arg admpass=${ADMINPASS} --build-arg OS=${OS} --build-arg HTTPS=${HTTPS} -v ${DATADIR}/bitwarden:/var/lib/bitwarden:Z -f ${DATADIR}bitwarden/project/Dockerfile"
+	;;
+	"n")
+	printf "to launch your built run the following command :\n"
+	printf "podman build --squash-all -t ${TAGNAME} --build-arg admpass=${ADMINPASS} --build-arg OS=${OS} -v ${DATADIR}/bitwarden:/var/lib/bitwarden:Z -f Dockerfile"
+	exit 0
+	;;
+	*)
+	printf "Invalid choice\n"
+	exit 1
+	;;
+esac
+#FILENAME="$(tr '[:upper:]' '[:lower:]' <<< ${OS})"-bitwarden.${VERSION}.oci
+#podman image save --format oci-archive -o /tmp/"${FILENAME}" localhost/"${TAGNAME}"
+#chown bitwarden: /tmp/"${FILENAME}"
+#sudo -u bitwarden podman image load -i /tmp/"${FILENAME}"
+#sudo -u bitwarden podman run -d --systemd=always --log-driver=journald --log-opt=tag=bitwarden --sdnotify=conmon --name bitwarden -h bitwarden.lan -v ${DATADIR}/bitwarden/bitwarden:/var/lib/bitwarden:rw -p 443:443 localhost/"${TAGNAME}"
+
